f5ae773e703af385d600d7e28d9570dade008f81
Brian Nutt <bnutt@usc.edu>
2014-11-23 14:59:17 -0800

    Created startup screen class.

---
+++ FinalProjectGUI/src/gui/StartUpScreen.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class StartUpScreen extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7055888233799252581L;
+	
+
+}
