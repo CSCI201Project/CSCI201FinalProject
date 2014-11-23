6c2d8cfbc4d1e47e6f2f99609ead87d7c32d49d2
Brian Nutt <bnutt@usc.edu>
2014-11-23 01:28:31 -0800

    Made class for waiting screen.

---
+++ FinalProjectGUI/src/gui/WaitingScreen.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class WaitingScreen extends JFrame{
+	public WaitingScreen(){
+		
+	}
+	
+	public static void main(String [] args){
+		new WaitingScreen();
+	}
+}
