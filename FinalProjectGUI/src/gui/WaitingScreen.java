1e33c05f6719e098c298f9501078370bef01ad6e
Brian Nutt <bnutt@usc.edu>
2014-11-23 11:39:08 -0800

    Added play button and pseudo code for making it so that only the host can click the button. That specific functionality has not been added yet.

--- FinalProjectGUI/src/gui/WaitingScreen.java
+++ FinalProjectGUI/src/gui/WaitingScreen.java
@@ -1,8 +1,18 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.*;
 
 public class WaitingScreen extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1164599519412930761L;
+
+	private JButton playButton;
 	public WaitingScreen(){
 		super("Waiting Screen");
 		this.setSize(800, 600);
@@ -14,6 +24,22 @@ public class WaitingScreen extends JFrame{
 	
 	public JPanel setupGUI(){
 		PanelBackground panel = new PanelBackground(1);
+		
+		/*Play Button, only Host should be able to click this, this will
+		 * have to be modified later so that this functionality is made
+		 * if(isHost){
+		 * playButton.setEnabled(true);
+		 * }
+		 * else{
+		 * playButton.setEnabled(false);
+		 * }
+		 */
+	    playButton = new JButton("Play");
+		playButton.setFont(new Font("Times New Roman",Font.BOLD,48));
+		playButton.setBounds(280, 0, 100, 100);
+		panel.add(playButton);
+		
+		
 		
 		
 		
