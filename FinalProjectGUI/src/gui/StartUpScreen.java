1af1fed7d78b6067463a4f8863f7074faa0cf5c3
Brian Nutt <bnutt@usc.edu>
2014-11-23 16:45:07 -0800

    Added label to display what type of character you are as well as a dummy image to test that the image will not overlap with any of the text.

--- FinalProjectGUI/src/gui/StartUpScreen.java
+++ FinalProjectGUI/src/gui/StartUpScreen.java
@@ -1,5 +1,9 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.*;
 
 public class StartUpScreen extends JFrame{
@@ -8,6 +12,47 @@ public class StartUpScreen extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = -7055888233799252581L;
-	
+	private JLabel characterLabel;
 
+	public StartUpScreen(){
+		super("Start Up Screen");
+		this.setSize(800, 600);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(setupGUI());
+		this.setVisible(true);
+	}
+	
+	public JPanel setupGUI(){
+		PanelBackground panel = new PanelBackground(2);
+		panel.setLayout(null);
+		
+		/*Label to inform client whether they are a zombie or a survivor*/
+		this.characterLabel = new JLabel("You are a " + "zombie!");
+		characterLabel.setForeground(Color.WHITE);
+		characterLabel.setFont(new Font("Times New Roman",Font.BOLD,32));
+		characterLabel.setBounds(300, 100, 275, 50);
+		panel.add(characterLabel);
+		
+		/*Image to display to client indicating displaying that they are a zombie or survior*/
+		ImageIcon temporaryImage = new ImageIcon("tempZombieSprite.png");
+		JLabel characterImageLabel = new JLabel(new ImageIcon(this.resizeImage(temporaryImage,100,100)));
+		characterImageLabel.setBounds(580, 100, 100, 100);
+		panel.add(characterImageLabel);
+		
+		return panel;
+	}
+	
+	/*takes in a ImageIcon, and the desired scaled dimensions, and returns the scaled image of the
+	 * image passed in*/
+	public Image resizeImage(ImageIcon backGround, int resizedImageWidth, int resizedImageHeight){
+		Image image = backGround.getImage();
+		Image newImage = image.getScaledInstance(resizedImageWidth, resizedImageHeight, Image.SCALE_SMOOTH);
+		backGround = new ImageIcon(newImage);
+		return backGround.getImage();
+	}
+	
+	public static void main(String [] args){
+		 new StartUpScreen();
+	}
 }
